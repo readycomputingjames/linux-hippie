354b152d64f8771a1af107de16a3e4e58474dc0b
James Hipp <51828166+readycomputingjames@users.noreply.github.com>
2019-08-08 01:27:00 -0400

    Update and rename wrapper.sh to hippie_mon_wrapper.sh

---
+++ hippie_mon_wrapper.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#########################################################################
+# James Hipp
+# System Support Engineer
+# Ready Computing
+#
+# Wrapper Script for Hippy-Mon to Send Email with Cron
+#
+# Sample Cron Setup:
+# [jhipp@test-server]$ crontab -l
+# # James Test (Run this Hourly)
+# 30 * * * * /path/to/hippie_mon_wrapper.sh
+#
+#
+#########################################################################
+
+/path/to/hippie_mon_dssh.sh /path/to/hosts.list '/path/to/hippie_mon.sh --rhel' |/usr/sbin/sendmail james.hipp@email.org
+
--- wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/path/to/hippie_mon_dssh.sh /path/to/hosts.list '/remote/path/to/hippie_mon.sh --rhel' |/usr/sbin/sendmail james.hipp@email.org
-
