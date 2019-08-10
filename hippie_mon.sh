550b48bf082b10c31ba4214cda543104dc185c54
James Hipp <51828166+readycomputingjames@users.noreply.github.com>
2019-08-10 10:21:45 -0400

    Update hippie_mon.sh

--- hippie_mon.sh
+++ hippie_mon.sh
@@ -25,6 +25,7 @@
 #
 # 20190808 = Converted this to run with multiples hosts using Hippie_Mon_DSSH
 # 20190808 = Updated to condense output for email text
+# 20190810 = Added divide by zero check for memory and swap
 #
 #########################################################################
 
@@ -75,9 +76,22 @@ rhel_mon()
    echo ""
 
    # Show Memory Utilization at this Point in Time
-   #/usr/bin/free -m
-   echo "-> Percentage of RAM in Use: `/usr/bin/free -m |grep Mem |awk '{print $3/$2 * 100.0}'`"
-   echo "-> Percentage of Swap in Use: `/usr/bin/free -m |grep Swap |awk '{print $3/$2 * 100.0}'`"
+   ram=`/usr/bin/free -m |grep Mem |awk '{print $3/$2 * 100.0}'`
+   swap=`/usr/bin/free -m |grep Swap |awk '{print $3/$2 * 100.0}'`
+
+   ### Check if Zero so we do not get a Divide by Zero Warning ###
+   if [ "$ram" = "0" ]
+   then
+      echo "-> Percentage of RAM in Use: 0.00"
+   else
+      echo "-> Percentage of RAM in Use: $ram"
+   fi
+   if [ "$swap" = "0" ]
+   then
+      echo "-> Percentage of Swap in Use: 0"
+   else
+      echo "-> Percentage of Swap in Use: $swap"
+   fi
 
    echo ""
 
